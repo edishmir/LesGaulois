859e3e5556c4464c052cb3e69aa9eb5e4efbef77
edishmir <edric.ishimwe-mirimo@univ-tlse3.fr>
2023-02-20 17:23:51 +0100

    Update

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,2 +1,29 @@
-package personnages;public class Druide {
+package personnages;
+
+import java.util.Random;
+
+public class Druide {
+    private String nom;
+    private int effetPotionMin;
+    private int effetPotionMax;
+    private int forcePotion = 1;
+
+    public Druide(String nom, int effetPotionMin, int getEffetPotionMax) {
+        this.nom = nom;
+        this.effetPotionMin = effetPotionMin;
+        this.effetPotionMax = getEffetPotionMax;
+        parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void parler(String texte) {
+        System.out.println(prendreParole() + "<< " + texte + " >>");
+    }
+
+    private String prendreParole() {
+        return "Le Druide " + nom + " : ";
+    }
 }
